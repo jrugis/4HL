8bac707032b2ac473139fc4af32fe0f3f2ec8d6a
jrugis <j.rugis@auckland.ac.nz>
2018-10-01 20:19:29 +1300

    initial commit

---
+++ Xmat2HLbin.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+#
+# mat2HLbin.py
+
+import matplotlib.cm as mp
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.io as sc
+import struct
+
+##################################################################
+# functions
+##################################################################
+
+def read_bin(fname):
+  f1 = open(fname, 'rb') # open the binary file
+  # get the vertices
+  nverts = struct.unpack('i', f1.read(4))[0]
+  verts = np.empty([nverts, 3])
+  for i in range(nverts):
+    verts[i] = struct.unpack('fff', f1.read(12))
+  f1.close # close the binary file 
+  return verts
+
+# get the reduced indices (for hololens vis)
+def get_reduced_indices(fname):
+  f1 = open(fname, 'rb')
+  nidx = struct.unpack('i', f1.read(4))[0]
+  idx = np.zeros((nidx, 1), dtype=np.int32)
+  for i in range(nidx):
+    idx[i] = struct.unpack('i', f1.read(4))[0]
+  f1.close
+  return idx
+
+# write for hololens vis data file
+def write_4HL(fname, verts, c_data, min, max, cm):
+  f1 = open(fname, 'wb')
+  f1.write(struct.pack('i', verts.shape[0]))
+  for x in verts:
+    f1.write(struct.pack('fff', x[0], x[1], x[2]))
+  f1.write(struct.pack('i', c_data.shape[1]))
+  for x in c_data:
+    f1.write(struct.pack('f', x[0]))
+  f1.write(struct.pack('f', min))
+  f1.write(struct.pack('f', max))
+  f1.write(struct.pack('i', cm.shape[0]))
+  for x in cm:
+    f1.write(struct.pack('fff', x[0], x[1], x[2]))
+  f1.close
+  return
+
+##################################################################
+# main program
+##################################################################
+
+dist_name = 'Ca_1_7.mat'
+dist_key = 'c_all'
+
+# read matlab data file
+print 'matlab data file: ' + dist_name 
+dist = sc.loadmat(dist_name)
+#print 'keys:', dist.keys()
+node_data = dist[dist_key]
+print node_data.shape
+
+# create a colormap
+ncolors = 1000
+cmap = mp.get_cmap("coolwarm",ncolors) # get a matplotlib color map
+cm = np.zeros([ncolors, 4])
+for i in range(ncolors): # convert colormap lut to numpy array
+  cm[i] = cmap(i)
+cm = cm[:,0:3] # don't need alpha
+
+# read cell data and write hololens files
+for cell_num in range(1,8):
+  print
+  print 'cell number: ', cell_num
+  fname = '4sim_out_N4_p3-p2-p4-' + str(cell_num) + 'tet.bin'
+  print 'mesh file: ' + fname
+  verts = read_bin(fname)
+
+  fname = 'reduced-indices_out_N4_p3-p2-p4-' + str(cell_num) + "tet.bin"
+  print 'reduced indices file: ' + fname
+  idx = get_reduced_indices(fname) # zero indexed
+  rverts = verts[idx[:,0]]
+  print 'vertex reduction:', verts.shape[0], '->', idx.shape[0]
+
+  c_data = node_data[0, cell_num-1]
+  c_data = c_data[idx[:,0]]
+  print 'min:', '{:0.3f}'.format(c_data.min()),
+  print 'max:', '{:0.3f}'.format(c_data.max())
+
+  fname = '4HL_Cell' + str(cell_num) + ".bin"
+  min = 0.0
+  max = 5.0
+  write_4HL(fname, rverts, c_data[:,500:1100], min, max, cm)
+  #plt.plot(np.transpose(c_data[0:10,:]))
+
+#plt.show()
+
---
+++ read_write.py
@@ -0,0 +1,151 @@
+import numpy as np
+import struct
+from evtk.hl import pointsToVTK
+from evtk.hl import unstructuredGridToVTK
+from evtk.vtk import VtkTriangle
+from evtk.vtk import VtkTetra
+
+###########################################################################
+# functions
+###########################################################################
+
+###########################################################################
+def read_lumen():
+  f1 = open('lumen_lines.txt', 'r') # open the lumen file
+
+  nlverts = int(f1.next().split()[0]) 
+  lverts = np.empty([nlverts, 3])
+  for i in range(nlverts): # get the lumen verts
+    lverts[i] = map(float, f1.next().split()[0:3])
+
+  nlines = int(f1.next().split()[0])
+  lines = np.empty([nlines, 2], dtype=int)
+  for i in range(nlines): # get the lumen line segments
+    lines[i] = map(int, f1.next().split()[0:2])
+
+  lsegs = np.empty([nlines, 6])
+  for i in range(nlines): # get the lumen line segments
+    v1 = map(float, lverts[lines[i,0]-1])
+    v2 = map(float, lverts[lines[i,1]-1])
+    lsegs[i] = np.array([v1, v2]).flatten()
+
+  f1.close()
+  return lsegs
+
+###########################################################################
+# read binary mesh
+def read_bin(fname):
+  f1 = open(fname, 'rb') # open the binary file
+
+  # get the vertices
+  nverts = struct.unpack('i', f1.read(4))[0]
+  verts = np.empty([nverts, 3])
+  for i in range(nverts):
+    verts[i] = struct.unpack('fff', f1.read(12))
+
+  # get the tris
+  ntris = struct.unpack('i', f1.read(4))[0]
+  tris = np.empty([ntris, 3], dtype=int)
+  for i in range(ntris):
+    tris[i] = struct.unpack('iii', f1.read(12))
+
+  # get the tets, dfa, dfb
+  ntets = struct.unpack('i', f1.read(4))[0]
+  tets = np.empty([ntets, 4], dtype=int)
+  dfa = np.empty([ntets])
+  dfb = np.empty([ntets])
+  for i in range(ntets):
+    tets[i] = struct.unpack('iiii', f1.read(16))
+    dfa[i] = struct.unpack('f', f1.read(4))[0]
+    dfb[i] = struct.unpack('f', f1.read(4))[0]
+
+  # get apical 
+  napical = struct.unpack('i', f1.read(4))[0]
+  apical = np.empty([napical], dtype=int)
+  for i in range(napical):
+    apical[i] = struct.unpack('i', f1.read(4))[0]
+
+  # get basal 
+  nbasal = struct.unpack('i', f1.read(4))[0]
+  basal = np.empty([nbasal], dtype=int)
+  for i in range(nbasal):
+    basal[i] = struct.unpack('i', f1.read(4))[0]
+
+  # get common
+  ncommon = struct.unpack('i', f1.read(4))[0]
+  common = np.empty([ncommon, 3], dtype=int)
+  for i in range(ncommon):
+    common[i] = struct.unpack('iii', f1.read(12))
+
+  f1.close # close the binary file 
+  return verts, tris, tets, dfa, dfb, apical, basal, common
+
+###########################################################################
+# write paraview points
+def write_points(fname, verts, pdata=None):
+  nverts = verts.shape[0]
+  xyz = np.empty([3, nverts]) # needs re-ordering
+  for i in range(nverts):
+    for j in range(3):
+      xyz[j,i] = verts[i,j]  
+  pointsToVTK(fname, \
+    xyz[0,:], xyz[1,:], xyz[2,:], \
+    data=pdata)  # write out vtu file
+  return
+
+###########################################################################
+# write paraview triangles
+def write_tris(fname, verts, tris, data=None):
+  nverts = verts.shape[0]
+  xyz = np.empty([3, nverts]) # because it needs re-ordering
+  for i in range(nverts):
+    for j in range(3):
+      xyz[j,i] = verts[i,j]  
+  ntris = tris.shape[0]
+  conn = np.empty(3*ntris)
+  for i in range(ntris):
+    conn[3*i] = tris[i,0]-1 # index from zero
+    conn[3*i+1] = tris[i,1]-1
+    conn[3*i+2] = tris[i,2]-1
+  offset = np.zeros(ntris, dtype=int)
+  for i in range(ntris):
+    offset[i] = 3*(i+1) 
+  ctype = np.zeros(ntris)
+  for i in range(ntris):
+    ctype[i] = VtkTriangle.tid 
+  unstructuredGridToVTK(fname, \
+    xyz[0,:], xyz[1,:], xyz[2,:], \
+    connectivity=conn, offsets=offset, cell_types=ctype, \
+    cellData=data, pointData=None)  # write out vtu file
+  return
+
+###########################################################################
+# write paraview tetrahedrons
+def write_tets(fname, verts, tets, data=None):
+  nverts = verts.shape[0]
+  xyz = np.empty([3, nverts]) # because it needs re-ordering
+  for i in range(nverts):
+    for j in range(3):
+      xyz[j,i] = verts[i,j]  
+  ntets = tets.shape[0]
+  conn = np.empty(4*ntets)
+  for i in range(ntets):
+    conn[4*i] = tets[i,0]-1 # index from zero
+    conn[4*i+1] = tets[i,1]-1
+    conn[4*i+2] = tets[i,2]-1
+    conn[4*i+3] = tets[i,3]-1
+  offset = np.zeros(ntets, dtype=int)
+  for i in range(ntets):
+    offset[i] = 4*(i+1) 
+  ctype = np.zeros(ntets)
+  for i in range(ntets):
+    ctype[i] = VtkTetra.tid 
+  unstructuredGridToVTK(fname, \
+    xyz[0,:], xyz[1,:], xyz[2,:], \
+    connectivity=conn, offsets=offset, cell_types=ctype, \
+    cellData=data, pointData=None)  # write out vtu file
+  return
+
+###########################################################################
+
+
